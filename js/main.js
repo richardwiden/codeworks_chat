c2cfd26b7ae587c9817860e04a9617db175bc9e3
Richard Widén <richardwiden@gmail.com>
2019-01-23 21:31:56 +0100

    Initial

--- js/main.js
+++ js/main.js
@@ -1,33 +1,73 @@
-let messageBox, chatContainer, messageContainer;
+let messageBox, chatContainer, messageContainer, lastMessage, lastScrollToAnimationElement;
 let ownerName = "richardwiden";
-let remoteName = "bot";
-let lastScrollToAnimationElement;
+const remoteName = "bot";
+const questions = ['How are you?', 'What\'s you name?', 'Where are you from?'];
+const answers = [
+    {
+        regex: /^(?=.*?\bhow\b)(?=.*?\bare\b)(?=.*?\byou\b).*/gmi,
+        answer: 'Im fine thank you'
+    },
+    {
+        regex: /^(?=.*?\bwhere\b)(?=.*?\bare\b)(?=.*?\bfrom\b).*/gmi,
+        answer: 'I was born in cyberspace'
+    }
+];
+
 $(function () {
-    console.log("ready!");
     chatContainer = $('#chat');
     messageContainer = $('#messages');
     messageBox = $('#messageBox');
+    setTimeout(submitAnswer, 5000);
+    console.log(JSON.stringify(answers));
 });
 
 function submitForm() {
-    let message = messageBox.val();
-    console.log(message);
-    writeMessage(message, "richardwiden");
+    lastMessage = messageBox.val().trim();
+    messageBox.val('');
+    writeMessage(lastMessage, "richardwiden");
 }
+
+function submitAnswer() {
+    let answer = getAnswer(lastMessage);
+    lastMessage = null;
+    let message = answer || getRandomQuestion();
+
+    writeMessage(message, remoteName);
+    setTimeout(submitAnswer, Math.floor(Math.random() * 5000 + 1));
+}
+
+function getAnswer() {
+    if (lastMessage === null || lastMessage === undefined) return null;
+    console.log('Finding answersfor:' + lastMessage);
+    for (let answer of answers)
+        if (lastMessage.match(answer.regex)) {
+            console.log('Found answer:' + JSON.stringify(answer));
+            return answer.answer;
+        }
+    console.log('Found no answer');
+    return null;
+}
+
+
+function getRandomQuestion() {
+    return questions[Math.floor(Math.random() * questions.length)];
+}
+
 
 function writeMessage(message, name) {
     let messageClass = name === ownerName ? "local-side" : "remote-side";
-    let chatbox = $('<div></div>').addClass(messageClass).addClass('chatbox')
-    chatbox.append($('<p></p>').text(message));
-    chatbox.append($('<p></p>').text(name).addClass('name'));
-    messageContainer.append(chatbox);
-    chatbox.fadeIn('slow');
-    scrollToElement(chatbox);
+    let event = $('<div></div>').addClass(messageClass).addClass('event')
+    event.append($('<p></p>').text(message));
+    event.append($('<p></p>').text(name).addClass('name'));
+    event.hide();
+    messageContainer.append(event);
+    event.fadeIn(500);
+    scrollToElement(event);
 }
 
-function scrollToElement(element){
-    if(lastScrollToAnimationElement) lastScrollToAnimationElement.stop();
-    lastScrollToAnimationElement= $('html, body').animate({
+function scrollToElement(element) {
+    if (lastScrollToAnimationElement) lastScrollToAnimationElement.stop();
+    lastScrollToAnimationElement = $('html, body').animate({
         scrollTop: element.offset().top
     }, 1000);
 }
